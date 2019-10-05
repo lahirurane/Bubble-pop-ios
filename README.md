f4e91f264dadd7f3e877191979f353d56ddfcf9c
Lahiru Rananasinghe <lahiru0561@gmail.com>
2019-10-05 10:32:52 +1000

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# BubblePop
+
+Action Puzzle Game for iOS
+
+![build status](https://travis-ci.com/uts-ios-dev/uts-ios-2019-bubble-pop-lahirurane.svg?token=JsubRadtNf7SExKc7AvK&branch=master)
+
+### Instructions
+
+See the [Project Description](./docs/Project%202%20Description.md)
+
+Place your project files in this directory.
---
+++ bubble-pop-lahirurane/bubble-pop-lahiruraneTests/bubble_pop_lahiruraneTests.swift
@@ -0,0 +1,34 @@
+//
+//  bubble_pop_lahiruraneTests.swift
+//  bubble-pop-lahiruraneTests
+//
+//  Created by Lahiru Ranasinghe on 30/4/19.
+//  Copyright © 2019 Lahiru Ranasinghe. All rights reserved.
+//
+
+import XCTest
+@testable import bubble_pop_lahirurane
+
+class bubble_pop_lahiruraneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
